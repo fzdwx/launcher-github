7a2e75cde341e65b9f7c992d4b94ee80a253132a
fzdwx <likelovec@gmail.com>
2023-12-04 23:02:34 +0800

    update

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import '@fzdwx/launcher-api/dist/style.css'
-import './assets/main.scss'
-
-import {createApp} from 'vue'
-// @ts-ignore
-import App from './App.vue'
-
-createApp(App).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,11 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import {defineConfig} from 'vite'
+import UnoCSS from 'unocss/vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+    server: {
+        port: 35678,
+    },
+    plugins: [react(), UnoCSS(),],
 })
